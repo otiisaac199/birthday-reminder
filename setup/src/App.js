a346403d5a3ae04284eacb91c841ef26ef38d4b4
otiisaac199 <otichinazaisaac19@gmail.com>
2022-09-28 13:03:03 +0100

    first commit

--- setup/src/App.js
+++ setup/src/App.js
@@ -2,17 +2,7 @@ import React, { useState } from "react";
 import data from "./data";
 import List from "./List";
 function App() {
-  const [people, setPeople] = useState(data);
-
-  return (
-    <main>
-      <section className="container">
-        <h3>0 birthdays today</h3>
-        <List people={people} />
-        <button onClick={() => console.log("you clicked me")}>clear all</button>
-      </section>
-    </main>
-  );
+  return <main>App</main>;
 }
 
 export default App;

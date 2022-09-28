1b721d95c28b53ea74fd885a1580900400801b78
otiisaac199 <otichinazaisaac19@gmail.com>
2022-09-28 13:36:45 +0100

    images: added person img

--- setup/src/App.js
+++ setup/src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <main>
       <section className="container">
-        <h3>0 birthdays today</h3>
+        <h3>{people.length} birthdays today</h3>
         <List people={people} />
         <button onClick={() => console.log("you clicked me")}>clear all</button>
       </section>

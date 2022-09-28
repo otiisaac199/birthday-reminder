e5f62b0795424b2112f21a1875fc7fab0b9d56a1
otiisaac199 <otichinazaisaac19@gmail.com>
2022-09-28 13:46:09 +0100

    static reminders done

--- setup/src/List.js
+++ setup/src/List.js
@@ -7,7 +7,11 @@ const List = ({ people }) => {
         const { id, name, age, image } = person;
         return (
           <article key={id} className="person">
-            <img src={name} alt={name} />
+            <img src={image} alt={name} />
+            <div>
+              <h4>{name}</h4>
+              <p>{age} years</p>
+            </div>
           </article>
         );
       })}
